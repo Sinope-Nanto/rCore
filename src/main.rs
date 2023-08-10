d448f9b60b283bdd5cf8027268812040e19d5a54
mhz <3440225591@qq.com>
2023-08-10 17:59:45 +0800

    hello world

---
+++ src/main.rs
@@ -0,0 +1,25 @@
+#![no_std]
+#![no_main]
+mod lang_items;
+mod sbi;
+mod console;
+
+use core::arch::global_asm;
+global_asm!(include_str!("entry.asm"));
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    clear_bss();
+    println!("Hello world!");
+    sbi::shutdown()
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize..ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}

f5b75a8c4996bb3b922e629f23bee31de89700db
Hammer Sara <3440225591@qq.com>
2023-08-10 23:14:58 +0800

    debug info output

--- src/lang_items.rs
+++ src/lang_items.rs
@@ -1,6 +1,19 @@
 use core::panic::PanicInfo;
+use crate::sbi::shutdown;
+// use crate::println;
+use crate::error;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> !{
-    loop {}
+fn panic(info: &PanicInfo) -> !{
+    if let Some(location) = info.location() {
+        error!(
+            "Panicked at {}:{} {}",
+            location.file(),
+            location.line(),
+            info.message().unwrap()
+        );
+    } else {
+        error!("Panicked: {}", info.message().unwrap());
+    }
+    shutdown()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 #![no_std]
 #![no_main]
+#![feature(panic_info_message)]
+
+mod console;
 mod lang_items;
 mod sbi;
-mod console;
+
 
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
@@ -10,8 +13,8 @@ global_asm!(include_str!("entry.asm"));
 #[no_mangle]
 pub fn rust_main() -> ! {
     clear_bss();
-    println!("Hello world!");
-    sbi::shutdown()
+    info!("Hello world!");
+    panic!("Shutdown machine!");
 }
 
 fn clear_bss() {
